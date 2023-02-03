79e1232305e934cf4543d0d460527d499ca09e07
DmytroRudenko11 <117588754+DmytroRudenko11@users.noreply.github.com>
2023-02-03 18:34:40 +0000

    Deploying to gh-pages from @ DmytroRudenko11/goit-js-hw-09@34c8ac8aa859240e6d2d584a158dcdd808d407df 🚀

---
+++ 01-color-switcher.2d34e02b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.body,o=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");o.addEventListener("click",n),e.addEventListener("click",onClickStop);var n=function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}}();
+//# sourceMappingURL=01-color-switcher.2d34e02b.js.map
