34c8ac8aa859240e6d2d584a158dcdd808d407df
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-02-03 20:34:16 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.body;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onClickStart);
+stopBtn.addEventListener('click', onClickStop);
+
+const onClickStart = () => {
+  body.style.backgroundColor = getRandomHexColor();
+};
