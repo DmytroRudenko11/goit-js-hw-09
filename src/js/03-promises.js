782bfd5556d84eb1a66510d7571f6d0804982ae3
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-02-05 21:29:02 +0200

    upd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,43 @@
 import Notiflix from 'notiflix';
 
+const form = document.querySelector('.form');
+form.addEventListener('submit', dataHandler);
+
+function dataHandler(e) {
+  e.preventDefault();
+  let currentDelay = 0;
+
+  const delay = Number(e.target.delay.value);
+  const step = Number(e.target.step.value);
+  const amount = Number(e.target.amount.value);
+
+  if (delay < 0) {
+    return Notiflix.Notify.failure(
+      'Value for first step should be positive or equal 0!'
+    );
+  } else if (0 >= amount) {
+    return Notiflix.Notify.failure(
+      'Amount of promises should be higher then 0!'
+    );
+  }
+
+  for (let i = 0; i < amount; i++) {
+    currentDelay = delay + step * i;
+
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
@@ -13,50 +51,3 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-
-const form = document.querySelector('.form');
-form.addEventListener('submit', dataHandler);
-
-function dataHandler(e) {
-  e.preventDefault();
-  let currentDelay = 0;
-
-  const {
-    elements: { delay, step, amount },
-  } = e.currentTarget;
-
-  if (Number(delay.value) < 0) {
-    return Notiflix.Notify.failure(
-      'Value for first step should be positive or equal 0!'
-    );
-  } else if (0 >= Number(amount.value)) {
-    return Notiflix.Notify.failure(
-      'Amount of promises should be higher then 0!'
-    );
-  }
-
-  for (let i = 0; i <= Number(amount.value) - 1; i++) {
-    currentDelay = Number(delay.value) + Number(step.value) * i;
-    console.log(
-      createPromise(i + 1, currentDelay)
-        .then(({ position, delay }) => {
-          Notiflix.Notify.success(
-            `✅ Fulfilled promise ${position} in ${delay}ms`
-          );
-        })
-        .catch(({ position, delay }) => {
-          Notiflix.Notify.failure(
-            `❌ Rejected promise ${position} in ${delay}ms`
-          );
-        })
-    );
-  }
-}
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
